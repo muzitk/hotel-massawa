b9eb0ff7fa01f1e384cf9c10044d216d77302aca
muzitk <msebhatlab1@gmail.com>
2019-06-26 22:13:13 -0700

    completed singleroom component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -32,12 +32,52 @@ class SingleRoom extends Component {
 
     const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
     
+    //to render 3 images only via array discructoring <StyledHero img={images[0] 
+    const [ mainImg, ...defaultImg] = images
+
   return (
-    <StyledHero img={images[0]}>
+    <>
+    <StyledHero img={mainImg||this.state.defaultBcg}>
     <Banner title={`${name} room`}>
       <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
       </Banner>
       </StyledHero>
+
+      <section className="single-room">
+        <div className="single-room-images">
+        {defaultImg.map((item,index)=>{
+         return <img key={index} src={item} alt={name}/>
+      })}
+      </div>
+
+      <div className ="single-room-info">
+        <article className="desc">
+          <h3>Details </h3>
+          <p> {description} </p>
+          </article>
+
+          <article className="info">
+              <h3>info </h3>
+              <h6>Price: ${price}</h6>       
+              <h6>Size: ${size} SQFT</h6> 
+              <h6>Price: ${price}</h6> 
+              <h6>Max Capacity: {capacity > 1? `${capacity} people` : `${capacity < 1 } person`}</h6>       
+              <h6> {pets ? "pets Allowed" : "no pets"}</h6>  
+              <h6>{breakfast && "Free Breakfast included"}</h6>       
+          </article> 
+        </div>
+      </section>
+      
+      <section classRoome="room-extras">
+        <h6> Extras</h6>
+        <ul className="extras">
+          {extras.map((item,index) => {
+            return <li key={index}>~ {item}</li>
+          })}
+        </ul>
+      </section>
+      </>
+
   );
   }
 }
