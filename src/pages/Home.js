03d45c45a0909765dfd03c3e5ec5239503e1042b
muzitk <msebhatlab1@gmail.com>
2019-06-26 18:15:43 -0700

    able to render images randomly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,6 @@ import FeaturedRooms from '../components/FeaturedRoooms'
 
 
 
-
 function Home () {
   return (
     <>
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import defaultBcg from '../images/room-1.jpg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
+import StyledHero from '../components/StyledHero'
 
-function SingleRoom () {
+
+class SingleRoom extends Component {
+  constructor(props) {
+    super(props)
+    //console.log(this.props)
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    }
+      
+  }
+  static contextType = RoomContext;
+  render() {
+    const {getRoom} = this.context;
+    const room = getRoom(this.state.slug)
+    if(!room){
+      return (
+         <div className="error">
+        <h3> room isnt available </h3>
+        <Link to="/rooms" className="btn-primary"> BAck to Rooms </Link>
+       </div>
+      )
+    }
+
+    const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+    
   return (
-    <div> hello from SingleRoom </div>
-
+    <StyledHero img={images[0]}>
+    <Banner title={`${name} room`}>
+      <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+      </Banner>
+      </StyledHero>
   );
+  }
 }
 export default SingleRoom;
